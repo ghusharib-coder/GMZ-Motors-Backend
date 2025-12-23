da9db2ec107507b6d61e8e971f0cb010b4c5028b
ghusharib-coder <ghusharibs@gmail.com>
2025-12-23 21:17:49 +0500

    i

--- routes/user.js
+++ routes/user.js
@@ -3,12 +3,12 @@ const router = express.Router();
 import User from '../models/UserSchema.js';
 router.post('/signup',async (req, res) => {
   try {
-    const { name, password} = req.body;
+    const { name, password,email,phone} = req.body;
     const userExist = await User.findOne({name});
     if (userExist) {
       return res.status(400).json({ message: "User already exists" });
     }
-    const user = await User.create({ name, password });
+    const user = await User.create({ name, password,email,phone});
     res.status(201).json({ message: "User registered successfully ✅", user });
   } catch (error) {
     res.status(500).json({ message: error.message });
