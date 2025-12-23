1f5a698b85dd95410e98c872d72ff92123dad2fd
ghusharib-coder <ghusharibs@gmail.com>
2025-12-23 20:04:08 +0500

    s

--- models/UserSchema.js
+++ models/UserSchema.js
@@ -4,6 +4,14 @@ const UserSchema= new mongoose.Schema({
         type:String,
         required:true  
     },
+    email:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:String,
+        required:true
+    },
     password:{
         type:String,
         required:true
